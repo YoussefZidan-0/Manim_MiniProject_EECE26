22795b05ec03b255a2e0750505beb77ca90a0184
YoussefZidan-0 <youssefzidan.eng@gmail.com>
2024-07-12 19:27:16 +0300

    Last Test

--- Manim.py
+++ Manim.py
@@ -18,8 +18,8 @@ class FrequencyBezierVisualization(Scene):
         group.set_color_by_gradient(RED, ORANGE, YELLOW_C, GREEN_D, BLUE)
         
 
-        c = NumberPlane().add_coordinates()
-        self.play(Write(c))
+        #c = NumberPlane().add_coordinates()
+        #self.play(Write(c))
         self.play(rect1.animate.move_to(ORIGIN),rect2.animate.move_to(ORIGIN))
         
         
@@ -27,4 +27,18 @@ class FrequencyBezierVisualization(Scene):
         s2=SurroundingRectangle(s1,color=WHITE)
         group2=VGroup(rect1, rect2)
         self.play(ReplacementTransform(group2, group), Write(s1), Write(s2))
+
+        t=Text("F1 F2 F3 F4 F5", font="sans", font_size=48).next_to(s2, direction=UP, buff=0.1)
+        self.play(Write(t))
+        
+        for j in range(0, len(t), 2):
+            self.play(Indicate(t[j], color=group[j//2].get_color()), 
+                  Indicate(t[j+1], color=group[j//2].get_color()), 
+                  Indicate(group[j//2], scale_factor=0.3, color=group[j//2].get_color()))
+            
+        g3=VGroup(group, s1, s2, t)
+        d=Dot()
+        self.play(ReplacementTransform(g3, d))
+        self.play(d.animate.scale(150))
+        self.play(FadeOut(d))
         self.wait(2)
